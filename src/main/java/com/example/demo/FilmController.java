e4726e8766f6874b2bc741cbe270f3b64dbc5498
shahmar <shahmar.a03@gmail.com>
2025-07-20 02:11:29 +0400

    new update

--- src/main/java/com/example/demo/FilmController.java
+++ src/main/java/com/example/demo/FilmController.java
@@ -10,11 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class FilmController {
 
     private final List<Film> film = List.of(
-            new Film("Inception", "Yuxu içində yuxu!"),
-            new Film("Matrix", "Reallıq və illüziya arasında seçim... \n İnsanlıq üçün gizli bir mübarizə başlayır."),
-            new Film("Forrest Gump", "Həyat bir qutu şokolad kimidir."),
-            new Film("The Godfather", "Ailə, güc və xəyanət...")
-    );
+            new Film("Inception", "A dream within a dream!"),
+            new Film("Matrix", "A choice between reality and illusion... \n A secret struggle begins for humanity."),
+            new Film("Forrest Gump", "Life is like a box of chocolates."),
+            new Film("The Godfather", "Family, power, and betrayal..."));
+
     private final Random random = new Random();
 
     @GetMapping("/film/random")
